d65d1bac1355bf9cd6e64c2d508933df44662988
fredericogutendorfer-gif <fredericogutendorfer@gmail.com>
2025-11-15 03:03:49 -0300

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -5,6 +5,29 @@ import {
   initializeApp 
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 
+<script type="module">
+  // Importar funções
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+  import { getFirestore, collection, addDoc, setDoc, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCSHUXa29ratZ5Cmo36jUOVew2pA82BG84",
+    authDomain: "rpg-fred.firebaseapp.com",
+    projectId: "rpg-fred",
+    storageBucket: "rpg-fred.firebasestorage.app",
+    messagingSenderId: "687646864182",
+    appId: "1:687646864182:web:0235bb545b5f397a45b997"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  export { auth, db, signInWithEmailAndPassword, onAuthStateChanged, signOut, collection, addDoc, setDoc, doc, onSnapshot };
+</script>
+
+
 import {
   getAuth,
   signInWithEmailAndPassword,
